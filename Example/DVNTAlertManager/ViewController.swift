7404e0a07b6642286a21580b993857f95e1df236
Raúl Vidal Muiños <raul.vidal@devinet.es>
2019-04-07 14:32:19 +0200

    Changes

--- Example/DVNTAlertManager/ViewController.swift
+++ Example/DVNTAlertManager/ViewController.swift
@@ -11,21 +11,48 @@ import DVNTAlertManager
 
 class ViewController: UIViewController
 {
+    fileprivate let alertManager = DVNTAlertManager.shared
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        self.alertManager.setInkColor(.white)
+        self.alertManager.setBaseColor(.green)
     }
     
     // MARK: - IBActions
     
     @IBAction func showIOSStyledAlertButtonAction(_ sender: Any)
     {
-        
+        self.alertManager.setAlertStyle(.iOS)
+        self.alertManager.showBasicAlert(title: "Hey", message: "This is an iOS styled alert")
     }
     
     @IBAction func showMaterialStyledAlertButtonAction(_ sender: Any)
     {
+        self.alertManager.setAlertStyle(.Android)
+        self.alertManager.showBasicAlert(title: "Hey", message: "This is a Material styled alert")
+    }
+    
+    @IBAction func showiOSLoadingIndicator(_ sender: Any)
+    {
+        self.alertManager.setAlertStyle(.iOS)
+        self.alertManager.showLoadingView(isUserinteractionEnabled: false)
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            self.alertManager.hideLoadingView()
+        }
+    }
+    
+    @IBAction func showMaterialLoadingIndicator(_ sender: Any)
+    {
+        self.alertManager.setAlertStyle(.Android)
+        self.alertManager.showLoadingView(isUserinteractionEnabled: false)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            self.alertManager.hideLoadingView()
+        }
     }
 }
 
